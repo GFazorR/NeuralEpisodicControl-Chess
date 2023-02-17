d14e65fd6dd25a2dd220759b49ec60967e28712a
GFazorR <comandantegr1@gmail.com>
2023-02-17 12:56:06 +0100

    added code for Replay Buffer

--- Memories.py
+++ Memories.py
@@ -1,6 +1,21 @@
+import random
+
+
 class ReplayBuffer:
-    def __init__(self):
-        pass
+    def __init__(self, max_length):
+        self.max_length = max_length
+        self.memory = list()
+
+    def __len__(self):
+        return len(self.memory)
+
+    def enqueue(self, transition):
+        self.memory.append(transition)
+        if len(self.memory) > self.max_length:
+            del self.memory[0]
+
+    def sample(self, sample_size):
+        return random.sample(self.memory, sample_size)
 
 
 class DifferentiableNeuralDictionary:
